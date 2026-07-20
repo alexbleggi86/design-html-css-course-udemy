2921d12fd648b9472b2ab5d58b44d70c9082d180
alexbleggi86 <alexbleggi86.dev@gmail.com>
2026-07-20 16:11:10 -0300

    implementing a sticky navigation bar

--- 09-omnifood-project-effects-optimizations-and-deployment/js/script.js
+++ 09-omnifood-project-effects-optimizations-and-deployment/js/script.js
@@ -55,6 +55,32 @@ allLinks.forEach(function (link) {
   });
 });
 
+// sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  },
+);
+
+obs.observe(sectionHeroEl);
+
 // fixing flexbox gap property missing in some safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
