cf5cc39074516f89ca94b8f7654ef15d54ab9688
alexbleggi86 <alexbleggi86.dev@gmail.com>
2026-07-20 11:24:33 -0300

    implementing smooth scrolling

--- 09-omnifood-project-effects-optimizations-and-deployment/js/script.js
+++ 09-omnifood-project-effects-optimizations-and-deployment/js/script.js
@@ -24,6 +24,37 @@ btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
+// smooth scrolling animation
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
 // fixing flexbox gap property missing in some safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
